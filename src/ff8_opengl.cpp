eab31ad931a735733bfcdb786464ab248e8e67a5
Martin Barker <37368+barkermn01@users.noreply.github.com>
2025-06-07 17:38:34 +0100

    Switched config option name to ff8_always_capture_input

--- src/ff8_opengl.cpp
+++ src/ff8_opengl.cpp
@@ -724,20 +724,20 @@ ff8_draw_menu_sprite_texture_infos_short *ff8_draw_icon_or_key6(int a1, ff8_draw
 
 int ff8_is_window_active()
 {
-	bool require_active_window = ff8_require_focus_for_input? gameHwnd == GetActiveWindow():true;
-	
-	if(require_active_window){
-		ff8_externals.engine_eval_keyboard_gamepad_input();
-		ff8_externals.has_keyboard_gamepad_input();
-	}
+	if (gameHwnd == GetActiveWindow() || ff8_always_capture_input){	
+		if(require_active_window){
+			ff8_externals.engine_eval_keyboard_gamepad_input();
+			ff8_externals.has_keyboard_gamepad_input();
+		}
 
-	if (simulate_OK_button)
-	{
-		// Flag the button OK as pressed
-		ff8_externals.engine_input_confirmed_buttons[1] = ff8_externals.engine_input_valid_buttons[1] = 0x40;
+		if (simulate_OK_button)
+		{
+			// Flag the button OK as pressed
+			ff8_externals.engine_input_confirmed_buttons[1] = ff8_externals.engine_input_valid_buttons[1] = 0x40;
 
-		// End simulation right here before we press this button by mistake in other windows
-		simulate_OK_button = false;
+			// End simulation right here before we press this button by mistake in other windows
+			simulate_OK_button = false;
+		}
 	}
 
 	return 0;
